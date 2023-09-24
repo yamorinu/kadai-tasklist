8768e39b4db1e9e29f51ab45564272e580cd5c25
yamori <ya10011458@yahoo.co.jp>
2023-09-24 03:49:05 +0000

    status 10

--- database/migrations/2023_09_19_061141_add_status_to_tasks_table.php
+++ database/migrations/2023_09_19_061141_add_status_to_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->String('status');
+            $table->String('status',10);
         });
     }
 
